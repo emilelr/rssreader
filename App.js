3e5743a347537fb981ae73b7868156a1f1e5ff99
emile <emile@2e-systems.com>
2020-11-02 23:03:59 +0100

    moved homescreen

--- App.js
+++ App.js
@@ -4,57 +4,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import * as rssParser from 'react-native-rss-parser';
+import HomeScreen from './src/screens/HomeScreen';
 import EntriesScreen from './src/screens/EntriesScreen';
 import entriesReducer from './src/redux/EntriesReducer';
 
 const store = createStore(entriesReducer);
-
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'Wired',
-    rss: 'https://www.wired.com/feed/rss'
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Slashdot',
-    rss: 'http://rss.slashdot.org/Slashdot/slashdot'
-  }
-];
-
-function HomeScreen({ navigation }) {
-
-  const renderItem = ({ item }) => (
-    <TouchableOpacity
-      style={styles.button}
-      onPress={() => { navigation.navigate('Entries') }}
-      >
-      <Text>{item.title}</Text>
-    </TouchableOpacity>
-  );
-
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
-  },
-});
 
 const Stack = createStackNavigator();
 
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, StatusBar } from 'react-native';
+
+const DATA = [
+  {
+    id: '34cf4554-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Nasa',
+    rss: 'http://www.nasa.gov/rss/dyn/breaking_news.rss'
+  },
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Wired',
+    rss: 'https://www.wired.com/feed/rss'
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Slashdot',
+    rss: 'http://rss.slashdot.org/Slashdot/slashdot'
+  }
+];
+
+function retrieve(link) {
+  return fetch('http://www.nasa.gov/rss/dyn/breaking_news.rss')
+    .then((response) => response.text())
+    .then((responseData) => rssParser.parse(responseData))
+    .then((rss) => {
+      console.log('rss', rss);
+      console.log(rss.title);
+      console.log(rss.items.length);
+      dispatch({
+        type: 'UPDATE_ENTRIES',
+        payload: rss.items
+      });
+    });
+}
+
+const renderItem = ({ item }) => (
+  <TouchableOpacity
+    style={styles.button}
+    onPress={() => {
+      console.log('retrieving');
+      retrieve(item.rss);
+      navigation.navigate('Entries')
+    }}
+    >
+    <Text>{item.title}</Text>
+  </TouchableOpacity>
+);
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+});
+
+export default HomeScreen;
