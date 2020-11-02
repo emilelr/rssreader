e5d1aae7a51e7da311da1865d9acdcfccef65353
emile <emile@2e-systems.com>
2020-11-02 21:22:52 +0100

    added list

--- App.js
+++ App.js
@@ -1,15 +1,59 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, StyleSheet, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Wired',
+    rss: 'https://www.wired.com/feed/rss'
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Slashdot',
+    rss: 'http://rss.slashdot.org/Slashdot/slashdot'
+  }
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
 function HomeScreen() {
+
+  const renderItem = ({ item }) => (
+      <Item title={item.title} />
+    );
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <FlatList
+      data={DATA}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+    />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
 
 const Stack = createStackNavigator();
 
